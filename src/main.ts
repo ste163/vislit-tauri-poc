2f063b11ed68fe7a2ff9ad441e4116d84bad97bc
Sam Edwards <samtedwards@pm.me>
2022-12-12 19:10:40 -0500

    added comments

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from "./app.vue";
 
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+// NOTE:
+// this imports ALL vuetify components and directives
+// would need to ensure the Vuetify Vite Plugin tree-shakes properly
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
