2db2d3be601541c9862c5fa7b95503b35e677dd4
Sam Edwards <samuel.edwards@techstars.com>
2022-12-17 19:38:03 -0500

    started dir checks; added notes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,20 @@
 )]
 
 fn main() {
+  use tauri::Manager;
   tauri::Builder::default()
+    .setup(|app| {
+      // this will show regardless of if it is dev or prod ENV
+      // so would need to add a check (i think)
+      // unless the cfg handles it?
+      #[cfg(debug_assertions)] // only include this code on debug builds
+      {
+        let window = app.get_window("main").unwrap();
+        window.open_devtools();
+        window.close_devtools();
+      }
+      Ok(())
+    })
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,63 @@
 <script setup lang="ts">
 import { invoke } from "@tauri-apps/api";
-import { appConfigDir, appDataDir } from "@tauri-apps/api/path";
+import {
+  exists,
+  createDir,
+  writeFile,
+  BaseDirectory,
+} from "@tauri-apps/api/fs";
+import { onMounted, ref } from "vue";
+
+// TODO:
+// add the Project type of Record<string, Project>
+const projects = ref<any>(null); // no project type yet
 
 // example of saving to the file system
 // https://www.matthewtao.com/blog/post/glipma-devlog-2/
 
-async function logAppConfigDir() {
-  const dir = await appConfigDir();
-  console.log("APP CONFIG DIR", dir);
+async function loadData() {
+  // currently tauri
+  // does not allow writing
+  // to arbitrary directories inside appDir
+  // like appDir/vislit-data
+  // so using the appDir/ with writing .json in there
+  // but MUST figure out how, or else it won't work.
+  // need the /projects/id/{date}.html
+  console.log("load data");
+  try {
+    const doesVislitDataExist = await exists("projects.json", {
+      dir: BaseDirectory.AppData,
+    });
+    if (doesVislitDataExist) {
+      console.log("vislit data exists!");
+      // read appDataDir, projects.json
+      // set that data to state
+    } else {
+      console.log("vislit data does NOT exist");
+      await writeFile("projects.json", "{}", {
+        dir: BaseDirectory.AppData,
+      });
+      // createDir vislit-data
+      // then create file projects.json inside of that
+      // then set projects to an empty {}
+      // as we know we will have no data in there
+    }
+
+    // const appDataDirPath = await appDataDir();
+    // const path = await join(appDataDirPath, 'users', 'tauri', 'avatar.png');
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function logAppDataDir() {
-  const dir = await appDataDir();
-  console.log("APP DATA DIR", dir);
-}
+// app data dir and app config dir default to the same location
+// what i'd need to do:
+// have a /vislit-data directory inside data dir
+// have a /vislit-data/window-settings.json that would
+// contain the last window position and size
+// that would be loaded by Rust backend (we don't want to set this
+// from inside vue because there'd be a lag)
+// when we get a CLOSE signal, you save the window position
 
 // now we can call our Command!
 // Right-click the application background and open the developer tools.
@@ -21,6 +65,10 @@ async function logAppDataDir() {
 invoke("greet", { name: "Im the vue app talking to backend!" })
   // `invoke` returns a Promise
   .then((response) => console.log(response));
+
+onMounted(async () => {
+  await loadData();
+});
 </script>
 
 <template>
@@ -34,25 +82,26 @@ invoke("greet", { name: "Im the vue app talking to backend!" })
     <v-main
       ><h1>What to test</h1>
 
-      <div class="mb-10">
-        <h2>Logging buttons</h2>
-        <button @click="logAppConfigDir">Log app config dir</button>
-        <button @click="logAppDataDir">Log app data dir</button>
+      <div class="mb-10 px-10">
+        <v-alert color="info">
+          Project data is:
+          <strong>{{ projects }}</strong>
+        </v-alert>
       </div>
 
-      <ol class="text-left">
-        <li>Writing JSON files with Rust API</li>
-        <li>Reading JSON from those files with Rust API</li>
-        <li>
+      <v-list class="text-left">
+        <v-list-item>Writing JSON files with Rust API</v-list-item>
+        <v-list-item>Reading JSON from those files with Rust API</v-list-item>
+        <v-list-item>
           Using Faker (or a similar project), create a simple way to generate
           project objects and update them. Like two or three dozen or so at a
           time so I can test how quickly tauri handles reading larger json
           files. Like 100mb would be ideal, potentially. Would want to display a
           count of the data + parts of the data. Along with some logging on
           speeds. Then test with a production version of tauri
-        </li>
-        <li>How does the frontend look across OSes?</li>
-      </ol>
+        </v-list-item>
+        <v-list-item>How does the frontend look across OSes?</v-list-item>
+      </v-list>
     </v-main>
   </v-layout>
 </template>
